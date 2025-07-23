5934d1ed918e733a6372242ec91b641a024ad9fc
dhruba-datta <dhrubadattaanjan@gmail.com>
2025-07-23 21:13:28 +0600

    contact updated

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -13,29 +13,47 @@ type ErrorType = Partial<FormType>;
 // Social links
 const socialLinks = [
   {
-    name: "GitHub",
-    url: "https://github.com/dhruba-datta",
+    name: 'Instagram',
+    url: 'https://www.instagram.com/dhrubz_/',
     icon: (
-      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-        <path d="M12 0C5.374 0 0 5.373 0 12 0 17.302 3.438 21.8 8.207 23.387c.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0112 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.3 24 12c0-6.627-5.373-12-12-12z"/>
+      <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+        <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5A4.25 4.25 0 0 0 20.5 16.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5zm4.25 3.25a5.25 5.25 0 1 1 0 10.5 5.25 5.25 0 0 1 0-10.5zm0 1.5a3.75 3.75 0 1 0 0 7.5 3.75 3.75 0 0 0 0-7.5zm5.25.75a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
       </svg>
     ),
   },
   {
-    name: "LinkedIn",
-    url: "https://www.linkedin.com/in/dhruba-datta/",
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/dhruba-datta/',
     icon: (
-      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-        <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+      <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+        <path d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-9h3v9zm-1.5-10.25c-.97 0-1.75-.78-1.75-1.75s.78-1.75 1.75-1.75 1.75.78 1.75 1.75-.78 1.75-1.75 1.75zm13.5 10.25h-3v-4.5c0-1.1-.9-2-2-2s-2 .9-2 2v4.5h-3v-9h3v1.25c.41-.59 1.09-1.25 2.09-1.25 2.21 0 2.91 1.46 2.91 3.36v5.64z" />
+      </svg>
+    ),
+  },
+    {
+    name: 'YouTube',
+    url: 'https://www.youtube.com/DhrubaDattaAnjan',
+    icon: (
+      <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+        <path d="M23.498 6.186a2.99 2.99 0 0 0-2.104-2.11C19.13 3.5 12 3.5 12 3.5s-7.13 0-9.394.576a2.99 2.99 0 0 0-2.104 2.11A31.13 31.13 0 0 0 0 12a31.13 31.13 0 0 0 .502 5.814 2.99 2.99 0 0 0 2.104 2.11C4.87 20.5 12 20.5 12 20.5s7.13 0 9.394-.576a2.99 2.99 0 0 0 2.104-2.11A31.13 31.13 0 0 0 24 12a31.13 31.13 0 0 0-.502-5.814zM9.75 15.02V8.98l6.5 3.02-6.5 3.02z" />
       </svg>
     ),
   },
   {
-    name: "Google Scholar",
-    url: "https://scholar.google.co.uk/citations?hl=en&user=RGxdIVkAAAAJ&view_op=list_works&authuser=1&gmla=AH70aAURqNk3ktsHQOlfZFUuNTBsHi7ZPnDjzbSkBWhg2ulKof0-hdur7ndSkkQ72sJwZ2ImrmCa9MtZLRPfjYoH",
+    name: 'Facebook',
+    url: 'https://www.facebook.com/dhruba.datta.anjan',
     icon: (
-      <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-        <path d="M5.242 13.769L0 9.5 12 0l12 9.5-5.242 4.269C17.548 11.249 14.978 9.5 12 9.5c-2.977 0-5.548 1.748-6.758 4.269zM12 10a7 7 0 1 0 0 14 7 7 0 0 0 0-14z"/>
+      <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+        <path d="M22.675 0h-21.35C.6 0 0 .6 0 1.326v21.348C0 23.4.6 24 1.326 24h11.495v-9.294H9.692v-3.622h3.129V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.797.143v3.24l-1.918.001c-1.504 0-1.797.715-1.797 1.763v2.313h3.587l-.467 3.622h-3.12V24h6.116C23.4 24 24 23.4 24 22.674V1.326C24 .6 23.4 0 22.675 0" />
+      </svg>
+    ),
+  },
+  {
+    name: 'Telegram',
+    url: 'https://t.me/dhruba_datta_anjan',
+    icon: (
+      <svg className="w-5 h-5" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+        <path d="M21.05 2.05a2.5 2.5 0 0 1 2.45 2.95l-2.1 13.5a2.5 2.5 0 0 1-3.6 1.7l-3.7-2.2-1.8 1.7a1.25 1.25 0 0 1-2.1-.7l-.7-3.6-3.7-1.5a1.25 1.25 0 0 1 .1-2.4l17-6.5a2.5 2.5 0 0 1 1.05-.2zm-2.1 2.7-13.5 5.2 3.7 1.5a1.25 1.25 0 0 1 .8 1l.7 3.6 1.8-1.7a1.25 1.25 0 0 1 1.4-.1l3.7 2.2a.5.5 0 0 0 .7-.3l2.1-13.5a.5.5 0 0 0-.3-.6z" />
       </svg>
     ),
   },
@@ -306,7 +324,14 @@ const Contact: React.FC = () => {
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label={social.name}
-                className="p-2 rounded-lg transition-all duration-200 hover:scale-110 text-gray-600 dark:text-gray-300 bg-gray-100 dark:bg-gray-800"
+                className={
+                  `p-2 rounded-lg transition-all duration-300 hover:scale-110 text-gray-500 dark:text-gray-400 bg-gray-100 dark:bg-gray-800 ` +
+                  (social.name === 'LinkedIn' ? 'hover:text-blue-600 dark:hover:text-blue-400' : '') +
+                  (social.name === 'Instagram' ? 'hover:text-pink-500 dark:hover:text-pink-400' : '') +
+                  (social.name === 'Telegram' ? 'hover:text-blue-400 dark:hover:text-blue-300' : '') +
+                  (social.name === 'YouTube' ? 'hover:text-red-500 dark:hover:text-red-400' : '') +
+                  (social.name === 'Facebook' ? 'hover:text-blue-700 dark:hover:text-blue-500' : '')
+                }
               >
                 {social.icon}
               </a>
